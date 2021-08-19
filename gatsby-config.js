481e67ab0a660d4049553edbe97d70c8ecb05d26
SkyeYoung <iskyex@outlook.com>
2021-08-19 23:41:38 +0800

    chore: add dependencies; add configs;

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,41 @@
+const path = require("path");
+
 module.exports = {
+  flags: {
+    PARALLEL_SOURCING: true,
+    DEV_WEBPACK_CACHE: true
+  },
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby-with-mui-v5",
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-plugin-react-helmet"
+    },
+    {
+      resolve: "gatsby-transformer-remark"
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: './docs',
+        path: path.resolve('./docs'),
       },
       __key: "pages",
+    },
+    {
+      resolve: 'gatsby-plugin-typegen',
+      options: {
+        outputPath: path.resolve(__dirname, 'src/__generated__/gatsby-types.d.ts'),
+        emitSchema: {
+          [path.resolve(__dirname, 'src/__generated__/gatsby-schema.graphql')]: true,
+          [path.resolve(__dirname, 'src/__generated__/gatsby-introspection.json')]: true,
+        },
+        emitPluginDocuments: {
+          [path.resolve(__dirname, 'src/__generated__/gatsby-plugin-documents.graphql')]: true,
+        },
+      },
     },
   ],
 };
