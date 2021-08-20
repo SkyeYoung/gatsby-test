44c1110abe44af5b9ec5594ef4ca9bab5628b242
SkyeYoung <iskyex@outlook.com>
2021-08-20 23:11:32 +0800

    chore: format gatsby-config and gatsby-node

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,41 @@
 const path = require("path");
 
 module.exports = {
-  flags: {
-    PARALLEL_SOURCING: true,
-    DEV_WEBPACK_CACHE: true
-  },
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-with-mui-v5",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-react-helmet"
+    flags: {
+        PARALLEL_SOURCING: true,
+        DEV_WEBPACK_CACHE: true
     },
-    {
-      resolve: "gatsby-transformer-remark"
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Gatsby",
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: './docs',
-        path: path.resolve('./docs'),
-      },
-      __key: "pages",
-    },
-    {
-      resolve: 'gatsby-plugin-typegen',
-      options: {
-        outputPath: path.resolve(__dirname, 'src/__generated__/gatsby-types.d.ts'),
-        emitSchema: {
-          [path.resolve(__dirname, 'src/__generated__/gatsby-schema.graphql')]: true,
-          [path.resolve(__dirname, 'src/__generated__/gatsby-introspection.json')]: true,
+    plugins: [
+        {
+            resolve: "gatsby-plugin-react-helmet"
         },
-        emitPluginDocuments: {
-          [path.resolve(__dirname, 'src/__generated__/gatsby-plugin-documents.graphql')]: true,
+        {
+            resolve: "gatsby-transformer-remark"
         },
-      },
-    },
-  ],
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: './docs',
+                path: path.resolve('./docs'),
+            },
+            __key: "pages",
+        },
+        {
+            resolve: 'gatsby-plugin-typegen',
+            options: {
+                outputPath: path.resolve(__dirname, 'src/__generated__/gatsby-types.d.ts'),
+                emitSchema: {
+                    [path.resolve(__dirname, 'src/__generated__/gatsby-schema.graphql')]: true,
+                    [path.resolve(__dirname, 'src/__generated__/gatsby-introspection.json')]: true,
+                },
+                emitPluginDocuments: {
+                    [path.resolve(__dirname, 'src/__generated__/gatsby-plugin-documents.graphql')]: true,
+                },
+            },
+        },
+    ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -21,27 +21,28 @@ exports.createPages = async ({actions, graphql, reporter}) => {
     const articleTemplate = path.resolve('src/templates/Article.tsx')
 
     const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            id
-            frontmatter {
-              path
+        {
+            allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date] }
+                limit: 1000
+            ) {
+                edges {
+                    node {
+                        id
+                        frontmatter {
+                            path
+                        }
+                        parent {
+                            ... on File {
+                                relativePath
+                            }
+                        }
+                    }
+                }
             }
-            parent {
-              ... on File {
-                relativePath
-              }
-            }
-          }
         }
-      }
-    }
-  `)
+    `)
+
     if (result.errors) {
         reporter.panicOnBuild(`Error while running GraphQL query.`)
         return
