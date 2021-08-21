a8a491663aa95a565693b4aef7d708fea806bfbc
SkyeYoung <iskyex@outlook.com>
2021-08-21 23:05:44 +0800

    feat: wrap link in article

---
+++ src/components/article/Link.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import MLink, {LinkProps as MLinkProps} from "@material-ui/core/Link";
+import {GatsbyLinkProps, Link as GLink} from "gatsby";
+import {removeIndex} from "../../utils/common";
+
+type StyledLinkProps<T extends React.Component = any> = MLinkProps & { component?: T }
+const StyledLink: React.FC<StyledLinkProps> = (props) => (
+    <MLink underline={'none'} {...props}/>
+)
+
+type Find = (func: ((exp: string) => boolean), ...exps: string[]) => string | undefined
+const find: Find = (func, ...exps) => exps.find((exp) => func(exp))
+
+type LinkProps = Omit<StyledLinkProps, 'href'> & Omit<GatsbyLinkProps<any>, 'to'> & { href: string }
+const Link: React.FC<LinkProps> = (props) => {
+    let {href, ...others} = props
+    const hasPrefix = !!find(href.startsWith.bind(href), 'http', '//')
+
+    // outside the website
+    if (hasPrefix) {
+        return <StyledLink href={href} target={'_blank'} rel={'noopener'} {...others}/>
+    } else {
+        // remove markdown extension
+        const MdExt = find(href.endsWith.bind(href), '.md', '.mdx', '.markdown', 'mdtext')
+        if (MdExt) href = href.slice(0, -MdExt.length)
+
+        // remove index
+        href = removeIndex(href)
+
+        return <StyledLink component={GLink} href={href} {...others}/>
+    }
+}
+
+export default Link
